1977914b2a2f55bae30f0f4b0342a2e393b79065
Marcos <mbcampolino@gmail.com>
2024-02-27 21:45:17 -0300

    configura entrypoints

--- mfe-2/src/app/app.module.ts
+++ mfe-2/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [CommonModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
